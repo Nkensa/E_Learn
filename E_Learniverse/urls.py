795669e9b0d9a3f051dbcd579642ac54a510cb76
Nkensa <nkensouphilou@gmail.com>
2024-05-17 16:06:34 +0100

    v.0.1

--- E_Learniverse/urls.py
+++ E_Learniverse/urls.py
@@ -3,4 +3,5 @@ from E_Learniverse.views import *
 
 urlpatterns = [
     path('welcome_app/', welcomeApp, name='welcome_app'),
+    path('profil/', profil, name='profil'),
 ]
--- E_Learniverse/views.py
+++ E_Learniverse/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from Account.models import CustomUser
+
 
 # Create your views here.
 
 def welcomeApp(request):
     selected = 'WelcomeApp'
+    # user = get_object_or_404(CustomUser)
 
-    return render(request, 'E_Learniverse/welcome_app.html', locals())
+    return render(request, 'E_Learniverse/welcome_app.html')
 
+
+def profil(request, pk):
+    selected = 'profil'
+    user = get_object_or_404(CustomUser, id_CustomUser=pk)
+
+    return render(request, 'E_Learniverse/profil.html', {'user': user})
